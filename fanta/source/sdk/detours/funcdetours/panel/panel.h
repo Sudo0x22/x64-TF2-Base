62560d3baef96c5f06382ba8ab803c3a4d93ba07
r็็็็็็็็็‏‏‎ ‎ ‏‏‎ ‎ ‏‏‎ ‎ ‏‏‎ ‎ ‏‏‎ ‎ ‏‏‎ ‎ ‏‏‎ ‎‏‏‎ ‎ ‏‏‎ ‎ ‏‏‎ ‏‏‎ ‎ ‏‏‎ ‎ ‏‏‎ ‎ ‏‏‎ ‎ ‏‏‎ ‎ ‏‏‎ ‎ ‏‏‎ ‎‏‏‎ ‎ ‏‏‎ ‎ ‏‏‎ ‎ ‏‏‎ ‎ ‏‏‎ ‎  ‏‏‎ ‎‏‏‎ ‎ ‎ ‏‏‎ ‎ ‏‏‎ ‎  ‏‏‎ ‎‏‏‎ ‎ <81118832+Sudo0x22@users.noreply.github.com>
2024-02-18 12:54:53 +0000

    Update

--- fanta/source/sdk/detours/funcdetours/panel/panel.h
+++ fanta/source/sdk/detours/funcdetours/panel/panel.h
@@ -13,7 +13,7 @@ namespace Hooks
 			constexpr uint32_t pIndex = 41u;
 			void __fastcall Detour(void* ecx, void* edx, unsigned int panel, bool force_rep, bool allow_force)
 			{
-				if (pHl2Interfaces.EngineClient->IsTakingScreenshot() && features::misc::clean_screenshots.Value) {
+				if (pHl2Interfaces.EngineClient->IsTakingScreenshot() && vars::misc::clean_screenshot) {
 					return Table.Original<Fn>(pIndex)(ecx, edx, panel, force_rep, allow_force);
 				}
 				Table.Original<Fn>(pIndex)(ecx, edx, panel, force_rep, allow_force);
